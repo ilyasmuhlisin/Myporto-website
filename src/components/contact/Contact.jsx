609e7810b5171e7f3bbb05d6dbaa0ce0335f23ec
ilyasmuhlisin <21ilyasmhlsn@gmail.com>
2022-08-06 18:08:26 +0700

    contact dan footer

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,84 @@
-import React from 'react'
-import './contact.css'
+import "./contact.css";
+import { MdOutlineMail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-  return (
-    <section id="contact">Contact</section>
-  )
-}
+  const form = useRef();
 
-export default Contact
+  const sendEmail = (e) => {
+    e.preventDefault();
+    // emailjs.sendForm(
+    //   "YOUR_SERVICE_ID",
+    //   "YOUR_TEMPLATE_ID",
+    //   form_current,
+    //   "YOUR_USER_ID"
+    // );
+    emailjs.sendForm(
+      "service_gm1rk9i",
+      "template_bq4ezm3",
+      form.current,
+      "vmFUGJywj1w4zvlWK"
+    );
+
+    e.target.reset();
+  };
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>ilyasmhlsn@gmail.com</h5>
+            <a href="mailto:ilyasmhlsn@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon" />
+            <h4>Messenger</h4>
+            <h5>ilyasmhlsn@gmail.com</h5>
+            <a href="mailto:ilyasmhlsn@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>+6281228892447</h5>
+            <a href="mailto:ilyasmhlsn@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          {/* End of contact options */}
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,49 @@
 import React from "react";
 import "./footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">Ilyas</a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portofolio">Portofolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://instagram.com">
+          <FiInstagram />
+        </a>
+        <a href="https://facebook.com">
+          <FaFacebookF />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; Ilyas Muhlisin. All rights reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
