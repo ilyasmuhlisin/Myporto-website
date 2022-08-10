3fe2f7c3d2498841c6352b554771f9b7b3287960
ilyasmuhlisin <21ilyasmhlsn@gmail.com>
2022-08-10 16:01:31 +0700

    fix content

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./footer.css";
 import { FaFacebookF } from "react-icons/fa";
 import { FiInstagram } from "react-icons/fi";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className="footer__logo">Ilyas</a>
+      <a href="#" className="footer__logo">
+        Ilyas
+      </a>
 
       <ul className="permalinks">
         <li>
@@ -32,11 +36,11 @@ const Footer = () => {
         </li>
       </ul>
       <div className="footer__socials">
-        <a href="https://instagram.com">
-          <FiInstagram />
+        <a href="https://www.linkedin.com/in/ilyas-muhlisin-31278a1b9/">
+          <BsLinkedin />
         </a>
-        <a href="https://facebook.com">
-          <FaFacebookF />
+        <a href="https://github.com/ilyasmuhlisin">
+          <FaGithub />
         </a>
       </div>
       <div className="footer__copyright">
